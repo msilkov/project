21fbc9d76401d105f5c63696b308dcd83784de00
Michael <m-silkov@inbox.ru>
2022-04-10 19:45:02 +0300

    upd smooth scroll script

--- js/main.js
+++ js/main.js
@@ -91,3 +91,22 @@ for (let item of formInputs) {
 		}
 	});
 }
+// smooth scroll
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+	link.addEventListener('click', function(e) {
+			e.preventDefault();
+
+			let href = this.getAttribute('href').substring(1);
+
+			const scrollTarget = document.getElementById(href);
+			const topOffset = 0; // если не нужен отступ сверху 
+			const elementPosition = scrollTarget.getBoundingClientRect().top;
+			const offsetPosition = elementPosition - topOffset;
+
+			window.scrollBy({
+					top: offsetPosition,
+					behavior: 'smooth'
+			});
+	});
+});
